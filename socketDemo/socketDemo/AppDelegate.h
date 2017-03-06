a1a924110f57855fe4112081d6f3977355372faa
Kaway <506158374@qq.com>
2017-03-06 19:17:21 +0800

    1.项目创建

---
+++ socketDemo/socketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  socketDemo
+//
+//  Created by 梁家伟 on 17/3/6.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ socketDemo/socketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socketDemo
+//
+//  Created by 梁家伟 on 17/3/6.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
